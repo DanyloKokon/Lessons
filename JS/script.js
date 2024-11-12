1be3966c2c109cac8a3a88e5a7c5aa3e02402212
DanyloKokon <149526893+DanyloKokon@users.noreply.github.com>
2024-11-12 18:36:50 +0100

    2less

---
+++ JS/script.js
@@ -0,0 +1,47 @@
+// const message = `okko ${firstName}`;
+// console.log(message.length )
+// console.log(message.toUpperCase() )
+// console.log(message.toLowerCase() )
+// console.log(message.indexOf('o'))
+// console.log(message.indexOf('O'))
+
+// const firstName = 'John ';
+// const secondName = 'Someoner'
+
+// console.log(firstName + secondName)
+
+
+// Напиши скрипт який виведе рядок в форматі:
+// «Гість x y поселяється в g номер q»,
+// підставив замість x y g q значення змінних
+
+// const firstName = 'Mango';
+// const lastName = 'Lee';
+// const room = 716;
+// const type = 'VIP';
+// const customersMsg = `Гість ${firstName} ${lastName} заселяється в ${type} номер ${room}`;
+// console.log(customersMsg);
+
+
+// const age = 13;
+// const myName = 'Dania';
+// const answer = `My name is ${myName} and i have ${age}`;
+// console.log(answer)
+
+
+// const blacklistedWord1 = 'спам';
+// const blacklistedWord2 = 'розпродаж';
+
+// const string1 = 'Привіт, я Марк Цукерберг, це не спам, пропоную тобі мільйон!';
+// const string2 = 'Найбільший РОЗПРОДАЖ цього тижня, не пропустіть!';
+// const string3 = 'Рекламна кампанія #jsIsAwesome';
+// console.log(string1.includes.toLocaleLowerCase()(blacklistedWord2.toLocaleLowerCase()))
+
+
+//const myName = 'Dania';
+//console.log(myName.replace('D', '@'))
+
+const message = 'To your balance added 1';
+const messageLanth = message.length;
+console.log(messageLanth);
+console.log(message.padEnd(26, '0'));
---
+++ JS/script1.js
@@ -0,0 +1,79 @@
+// let resultOne = "25" - 5;
+// console.log(resultOne);
+// console.log(typeof resultOne)
+
+// let resultTwo = 10 * "80";
+// console.log(resultTwo)
+// console.log(typeof resultTwo)
+
+// let resultOne = 2 - 1 * 5;
+// console.log(resultOne);
+// console.log(typeof resultOne)
+
+
+// let resultThree = 2 - 1 + 5;
+// console.log(resultThree);
+// console.log(typeof resultThree)
+
+
+// let a = 5;
+// let b = 10;
+// let c = 15;
+// let d = 20;
+// a += 2;
+// b -= 4;
+// c *= 3;
+// d /= 10;
+
+// const age = 18;
+// const isAdult= age >= 18;
+
+
+
+// const correctPassword = "jqueryismyjam";
+// const userPassword = "mangodab3st";
+// console.log(correctPassword == userPassword);
+
+// let a = 1 + 2;
+// let b = 2;
+
+// let result = 8 - (a = b + 3);
+// console.log(result);
+
+
+
+
+
+
+
+// -------------------------------Calculator
+
+// let base1;
+// let power2;
+
+// base1 = prompt('Enter the number 1');
+// base1 =  Number(base1);
+
+// power2 = prompt('Enter the degree');
+// power2 = Number(power2);
+
+// console.log(base1);
+// console.log(power2)
+
+// const result = base1 ** power2;
+// console.log(result);
+
+// alert("Your number is: " + result );
+
+
+
+//--------------------------------- Task 1
+
+let randomAnser = Math.random() * (80 - 10) + 10;
+console.log(randomAnser);
+
+
+let numThree = 584.49;
+numThree = Math.round(numThree)
+numThree = numThree / 100
+console.log(numThree)
