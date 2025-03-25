0df66f9737c98d60e7568f9e305a5809ec898379
DanyloKokon <149526893+DanyloKokon@users.noreply.github.com>
2025-03-25 18:10:21 +0100

    yug

--- JS/events.js
+++ JS/events.js
@@ -1,75 +1,106 @@
-const targetBtn = document.querySelector('.js-target-btn');
-const addListenerBtn = document.querySelector('.js-add-listener');
-const removeListenerBtn = document.querySelector('.js-remove-listener');
+// const targetBtn = document.querySelector('.js-target-btn');
+// const addListenerBtn = document.querySelector('.js-add-listener');
+// const removeListenerBtn = document.querySelector('.js-remove-listener');
 
-addListenerBtn.addEventListener('click', () => {
-    targetBtn.addEventListener('click', () => {
-        console.log('Works');
-    });
+// addListenerBtn.addEventListener('click', () => {
+//     targetBtn.addEventListener('click', () => {
+//         console.log('Works');
+//     });
+// })
+
+// removeListenerBtn.addEventListener('click', () => {
+//     targetBtn.removeEventListener('click', () => {
+//         console.log('Works end');
+//     });
+// });
+
+// /*
+//  * Події.
+//  * - Створення та видалення слухача
+//  * - Назви колбеків для слухачів
+//  *    - handle*: handleSubjectEvent
+//  *    - *Handler: subjectEventHandler
+//  *    - on*: onSubjectEvent
+//  * - Посилальна ідентичність колбеків
+//  * - Об'ект Події
+//  */
+
+
+// const products = [
+//     {
+//         name: 'Товар-2',
+//         description:
+//             'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa eius deleniti dolor facilis consectetur laborum aperiam exercitationem quam hic natus.',
+//         price: 3000,
+//         available: false,
+//         onSale: false,
+//     },
+//     {
+//         name: 'Товар-3',
+//         description:
+//             'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa eius deleniti dolor facilis consectetur laborum aperiam exercitationem quam hic natus.',
+//         price: 1500,
+//         available: true,
+//         onSale: false,
+//     },
+//     {
+//         name: 'Товар-4',
+//         description:
+//             'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa eius deleniti dolor facilis consectetur laborum aperiam exercitationem quam hic natus.',
+//         price: 2500,
+//         available: false,
+//         onSale: false,
+//     },
+// ];
+
+
+
+// function createArticles (articles){
+//    return articles.map((product) => {
+//     return `<article class="product">
+//        <h2 class="product__name">${product.name}</h2>
+//        <p class="product__descr">${product.description}</p>
+//      <p product__pridct>Ціна: ${product.price} кредитів</p>
+//       </article>`
+// }).join(' ')}
+// console.log(articles);
+
+
+// articleEl.innerHTML = articles.join(' ')
+
+// const addButton = document.querySelector('.js-add-product');
+// const addGallary = document.querySelector('.ja-gallery');
+// addButton.addEventListener('click', () => {
+//     addGallary.insertAdjacentHTML('beforeend', createArticles(products));
+//});
+
+
+
+
+// ----------------------------------------------------------------------
+const inputEl = document.querySelector('.js-input');
+const chekboxEl = document.querySelector('.js-license');
+const buttonEl = document.querySelector('.js-button');
+const spanEl = document.querySelector('.js-button span');
+const soundEl = document.querySelector('.js-sound');
+//inputEl.addEventListener('input', (event) => {
+//console.log(event.currentTarget.value);
+//spanEl.textContent = event.currentTarget.value;
+//})
+chekboxEl.addEventListener('change', (event) => {
+    // if (event.currentTarget.checked) {
+    //     buttonEl.removeAttribute('disabled');
+    // } else {
+    //     buttonEl.setAttribute('disabled', true);
+    // }
+    buttonEl.disabled = !event.currentTarget.checked;
 })
 
-removeListenerBtn.addEventListener('click', () => {
-    targetBtn.removeEventListener('click', () => {
-        console.log('Works end');
-    });
-});
+inputEl.addEventListener('blur', (event) => {
+console.log(event.currentTarget.value);
+})
 
-/*
- * Події.
- * - Створення та видалення слухача
- * - Назви колбеків для слухачів
- *    - handle*: handleSubjectEvent
- *    - *Handler: subjectEventHandler
- *    - on*: onSubjectEvent
- * - Посилальна ідентичність колбеків
- * - Об'ект Події
- */
+soundEl.addEventListener('change', (event) => {
+console.log(event.currentTarget.value);
 
-
-const products = [
-    {
-        name: 'Товар-2',
-        description:
-            'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa eius deleniti dolor facilis consectetur laborum aperiam exercitationem quam hic natus.',
-        price: 3000,
-        available: false,
-        onSale: false,
-    },
-    {
-        name: 'Товар-3',
-        description:
-            'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa eius deleniti dolor facilis consectetur laborum aperiam exercitationem quam hic natus.',
-        price: 1500,
-        available: true,
-        onSale: false,
-    },
-    {
-        name: 'Товар-4',
-        description:
-            'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa eius deleniti dolor facilis consectetur laborum aperiam exercitationem quam hic natus.',
-        price: 2500,
-        available: false,
-        onSale: false,
-    },
-];
-
-
-
-function createArticles (articles){
-   return articles.map((product) => {
-    return `<article class="product">
-       <h2 class="product__name">${product.name}</h2>
-       <p class="product__descr">${product.description}</p>
-     <p product__pridct>Ціна: ${product.price} кредитів</p>
-      </article>`
-}).join(' ')}
-console.log(articles);
-
-
-articleEl.innerHTML = articles.join(' ')
-
-const addButton = document.querySelector('.js-add-product');
-const addGallary = document.querySelector('.ja-gallery');
-addButton.addEventListener('click', () => {
-    addGallary.insertAdjacentHTML('beforeend', createArticles(products));
-});
+})
